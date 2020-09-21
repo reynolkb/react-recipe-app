db3cbcfd2adfc488735ecbf9657803ed19e11f36
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2020-09-20 17:17:25 -0700

    Updates

---
+++ precache-manifest.abd3437fab7eef8306c10733ff4aef75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52fab38062355bb4546a1880d6cb3ba4",
+    "url": "/react-recipe-app/index.html"
+  },
+  {
+    "revision": "63113e35c2b98974e148",
+    "url": "/react-recipe-app/static/css/main.f0694cee.chunk.css"
+  },
+  {
+    "revision": "b9b23b05c9bf24684e23",
+    "url": "/react-recipe-app/static/js/2.70eb02d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-recipe-app/static/js/2.70eb02d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63113e35c2b98974e148",
+    "url": "/react-recipe-app/static/js/main.6311ac10.chunk.js"
+  },
+  {
+    "revision": "209bb40ddcfb53baae52",
+    "url": "/react-recipe-app/static/js/runtime-main.84029532.js"
+  }
+]);
